ef44ae33bc2d9c11535f764d69e99f78102e17af
leonnardo.spereira <leonnardo.spereira@b2wdigital.com>
2023-04-08 19:43:34 -0300

    Ajuste utils

--- workflow/config/utils.py
+++ workflow/config/utils.py
@@ -1,4 +1,5 @@
 import pendulum
+from airflow.models import Variable
 
 SAO_PAULO_TZ = pendulum.timezone("America/Sao_Paulo")
 ROOT_PATH = Variable.get("ROOT_PATH", default_var="/home/airflow/gcs/dags/")
