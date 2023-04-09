e3d9a5b578b9bf9aa889f1e5d1d6cad7d8ceb02b
leonnardo.spereira <leonnardo.spereira@b2wdigital.com>
2023-04-09 02:55:34 -0300

    feature/add_dags_tables

---
+++ workflow/sql/sensor/vendas_marca_ano_mes.py
@@ -0,0 +1,11 @@
+VENDAS_MARCA_ANO_MES = f"""
+         SELECT COUNT(*) = 1 AS should_continue
+                 FROM `singular-arcana-383119.refined.__TABLES__` 
+                     WHERE row_count > 0 
+                       AND CAST(TIMESTAMP_MILLIS(last_modified_time) AS DATE) = CURRENT_DATE('America/Sao_Paulo')
+                       AND table_id  = 'base_vendas_anos'
+"""
+
+SENSOR_QUERIES = {
+    "vendas_marca_ano_mes" : VENDAS_MARCA_ANO_MES
+}
