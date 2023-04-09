544e5e4195b6338ce08f668729f14e18f5c6596f
leonnardo.spereira <leonnardo.spereira@b2wdigital.com>
2023-04-09 18:42:15 -0300

    fix/adjust_task

--- workflow/dags/gb_insert_raw_api_data.py
+++ workflow/dags/gb_insert_raw_api_data.py
@@ -35,49 +35,39 @@ with DAG(
     tags=["Leonnardo Pereira", "insert", "raw", "api"],
 ) as dag:
     
-    @task_group("api_to_storage", default_args=default_args)
     def api_to_storage():
-        client_id = SPOTIFY_CLIENT_ID_API
-        client_secret = SPOTIFY_CLIENT_SECRET_API
-
         def get_auth_header(token):
             return {'Authorization': f'Bearer {token}'}
 
         @task(task_id="get_token", default_args=default_args)
-        def get_token():
+        def get_token(client_id, client_secret):
             auth_string = client_id + ":" + client_secret
             auth_bytes = auth_string.encode("utf-8")
             auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
-
             url = 'https://accounts.spotify.com/api/token'
-
             headers = {
                 'Authorization': f'Basic {auth_base64}',
                 'Content-type': 'application/x-www-form-urlencoded'
-            }
-
+                }
             data = {'grant_type': 'client_credentials'}
-
             response = requests.post(url, headers=headers, data=data)
             json_result = json.loads(response.content)
             token = json_result["access_token"]
 
             return token
-        
+
         @task(task_id="search_podcast", default_args=default_args)
         def search_podcast(token, term, type):
             url = 'https://api.spotify.com/v1/search'
             headers = get_auth_header(token)
             query_string = f"?q={term}&type={type}&limit=50&market=BR"
-
             response = requests.get(url+query_string, headers=headers)
             json_result = json.loads(response.content)
-
             if len(json_result['shows']['items']) == 0:
                 pass
             else:
                 return json_result
-            
+
         @task(task_id="insert_json_to_storage", default_args=default_args)
         def insert_json_to_storage(json_results, term: str, type, bucket):
             if json_results:
@@ -89,22 +79,20 @@ with DAG(
                 blob.upload_from_string(data=data)
             else:
                 pass
-    
+
             return
-        
+
         @task(task_id="search_podcast_episodes", default_args=default_args)
         def search_podcast_episodes(token, id):
             url = f"https://api.spotify.com/v1/shows/{id}?market=BR"
             headers = get_auth_header(token)
-
             response = requests.get(url, headers=headers)
             json_result = json.loads(response.content)
-
             if len(json_result) == 0:
                 pass
             else:
                 return json_result
-            
+
         @task(task_id="insert_json_episodes_to_storage", default_args=default_args)
         def insert_json_episodes_to_storage(json_results, term, bucket):
             if json_results:
@@ -118,21 +106,16 @@ with DAG(
                 pass
             
             return
-        
-        @task(task_id="main", default_args=default_args)
-        def main():
-            term = "data+hackers"
-            bucket = "raw_data_boticario"
-            type = "show"
-            id = "1oMIHOXsrLFENAeM743g93"
 
-            token = get_token()
-            results_podcast = search_podcast(token, term, type)
-            result_episodes = search_podcast_episodes(token, id)
+        client_id = SPOTIFY_CLIENT_ID_API
+        client_secret = SPOTIFY_CLIENT_SECRET_API
+        term = "data+hackers"
+        bucket = "raw_data_boticario"
+        type = "show"
+        id = "1oMIHOXsrLFENAeM743g93"
+        token = get_token(client_id, client_secret)
 
-            insert_json_to_storage(results_podcast, term, type, bucket)
-            insert_json_episodes_to_storage(result_episodes, term, bucket)
+        insert_json_to_storage(search_podcast(token, term, type), term, type, bucket)
+        insert_json_episodes_to_storage(search_podcast_episodes(token, id), term, bucket)
 
-        main()
-
-api_to_storage()
+    api_to_storage()
