c6da3123c1fd7c76f66188c0b4ccafb594bca2a7
leonnardo.spereira <leonnardo.spereira@b2wdigital.com>
2023-04-09 22:22:06 -0300

    fix/sql

--- workflow/sql/load/vendas_marca_linha.sql
+++ workflow/sql/load/vendas_marca_linha.sql
@@ -4,5 +4,5 @@ SELECT
   SUM(QTD_VENDA) AS SOMA_VENDA,
   CURRENT_DATETIME('America/Sao_Paulo') AS datahora_carga
 FROM
-  `singular-arcana-383119.refined.base_vendas_anos`
+  `singular-arcana-383119.refined.base_venda_ano`
 GROUP BY 1,2
